55cd483b0da9d459f2033962eaf14387457d6c37
Akash Kandasamy <akashram953@gmail.com>
2022-10-04 19:56:55 +0530

    improvement of Home Page

--- lib/widgets/HomeCard.dart
+++ lib/widgets/HomeCard.dart
@@ -4,6 +4,7 @@ import 'package:amuse/utilities/doodle.dart';
 import 'package:amuse/utilities/jokes.dart';
 import 'package:amuse/utilities/quotes.dart';
 import 'package:amuse/utilities/username.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeCard extends StatefulWidget {
   const HomeCard({
@@ -26,18 +27,54 @@ class HomeCard extends StatefulWidget {
 class _HomeCardState extends State<HomeCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-        elevation: 20,
-        color: widget.color,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(35),
-        ),
-        child: ListTile(
-          leading: Icon(widget.icon),
-          title: Text(widget.title),
-          subtitle: Text(widget.subtitle),
-          onTap: (openScreen),
-        ));
+    return SizedBox(
+      height: 100,
+      child: Card(
+          margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+          elevation: 10,
+          color: widget.color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: InkWell(
+            onTap: (openScreen),
+            child: Container(
+              height: 50,
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(
+                      widget.icon,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.title,
+                            style: GoogleFonts.rubik(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 20),
+                          ),
+                          Text(
+                            widget.subtitle,
+                            style: GoogleFonts.rubik(color: Colors.white),
+                          ),
+                        ])
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
   }
 
   openScreen() {
