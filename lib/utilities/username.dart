c4428fe1c622327aa8f3ac9d9be49928934d45a6
Akash Kandasamy <akashram953@gmail.com>
2022-10-08 20:21:30 +0530

    Added username utility

--- lib/utilities/username.dart
+++ lib/utilities/username.dart
@@ -9,8 +9,13 @@ class Username extends StatefulWidget {
 }
 
 class _UsernameState extends State<Username> {
-  String? _username;
+  String _firstName = "";
+  String _lastName = "";
+  final List<String> _adj = [];
+
   var generator = UsernameGenerator();
+  final _firstNameController = TextEditingController();
+  final _lastNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,33 +23,56 @@ class _UsernameState extends State<Username> {
       appBar: AppBar(
         title: const Text('Username Generator'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _username = generator.generateRandom();
-                  });
-                },
-                child: const Text("Generate random username.")),
-            printUsername()
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(
+                  labelText: 'First Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _lastNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Last Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _firstName = _firstNameController.text;
+                      _lastName = _lastNameController.text;
+                    });
+                  },
+                  child: const Text("Generate random username.")),
+              printUsername(_firstName, _lastName)
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget printUsername() {
-    if (_username?.isEmpty ?? true) {
-      return Text("");
+  Widget printUsername(String _firstName, String _lastName) {
+    var _genUsername =
+        generator.generateForName(_firstName, lastName: _lastName);
+    if (_firstName.isEmpty) {
+      return const Text("First Name Cannot be Empty");
     }
-    return Container(
-        padding: const EdgeInsets.fromLTRB(50, 10, 50, 0),
-        child: Text(
-          'Generated username : $_username',
-          style: Theme.of(context).textTheme.bodyMedium,
-        ));
+    if (_lastName.isEmpty) {
+      return const Text("Last Name Cannot be Empty");
+    }
+    return Text(
+      "Your Generated Username: $_genUsername",
+      style: const TextStyle(fontSize: 20),
+    );
   }
 }
