c620492f8094dabc0e3ce0c27c0b34f1565009d4
prackode <prackode@gmail.com>
2022-10-31 17:55:45 +0530

    made suitable changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
+
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:amuse/HomePage.dart';
-import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ThemeClass {
   static ThemeData lightTheme = ThemeData(
@@ -40,19 +42,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SplashScreenPage extends StatelessWidget {
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenPageState createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: const HomePage(),
-      backgroundColor: Color(0xFF2A2E30),
-      image: Image.asset(
-        "assets/images/AMUSE.png",
-        fit: BoxFit.contain,
+    return Container(
+      color: Color(0xFF2A2E30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            child: Image.asset(
+              "assets/images/AMUSE.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20.0),
+            child: SpinKitCubeGrid(
+              size: 50,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
-      photoSize: 150.0,
-      loaderColor: Color.fromARGB(255, 126, 126, 126),
     );
   }
 }
